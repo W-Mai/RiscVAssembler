8d15ad4d07abf2f43ef2abfd0a0a13761f93d04b
W-Mai <1341398182@qq.com>
2022-05-05 23:52:14 +0800

    增加：addi、xori、ori、andi、slli、srli、srai、slti、sltiu

--- RiscVAssembler/ASMInst.py
+++ RiscVAssembler/ASMInst.py
@@ -31,9 +31,18 @@ class ASMInstBase(object):
                 f"{self.func3:03b}",
                 f"{self.rd:05b}",
                 f"{self.opcode:07b}"])
+        if self.inst_type == 'I':
+            return bit_sep.join([
+                f"{self.imm:012b}",
+                f"{self.rs1:05b}",
+                f"{self.func3:03b}",
+                f"{self.rd:05b}",
+                f"{self.opcode:07b}"])
 
     def comment(self):
         if self.inst_type == 'R':
+            return f"{self.inst_name:8}" + '\t'.join([f"{inst}: {getattr(self, inst)}" for inst in self.inst_args])
+        if self.inst_type == 'I':
             return f"{self.inst_name:8}" + '\t'.join([f"{inst}: {getattr(self, inst)}" for inst in self.inst_args])
 
     def description(self):
@@ -208,3 +217,170 @@ class SLTU(ASMInstBase):
 
     def description(self):
         return f"R{self.rd} = (R{self.rs1} < R{self.rs2}) ? 1 : 0"
+
+
+class ADDI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "addi"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b000
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} + {self.imm}"
+
+
+class XORI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "xori"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b100
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} ^ {self.imm}"
+
+
+class ORI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "ori"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b110
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} | {self.imm}"
+
+
+class ANDI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "andi"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b111
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} & {self.imm}"
+
+
+class SLLI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "slli"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b001
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} << {self.imm}"
+
+    def _build(self):
+        super()._build()
+        self.imm = 0b000_0000_11111 & self.imm
+
+
+class SRLI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "srli"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b101
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} >> {self.imm}"
+
+    def _build(self):
+        super()._build()
+        self.imm = 0b000_0000_11111 & self.imm
+
+
+class SRAI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "srai"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b101
+        self.func7 = 0b0100000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = R{self.rs1} >> {self.imm}"
+
+    def bitcode(self, bit_sep=''):
+        self.imm = 0b000_0000_11111 & self.imm | 0b010_0000_00000
+        bits = super().bitcode(bit_sep)
+        self.imm = self.imm & 0b000_0000_11111
+        return bits
+
+
+class SLTI(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "slti"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b010
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = (R{self.rs1} < {self.imm}) ? 1 : 0"
+
+
+class SLTIU(ASMInstBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.inst_type = "I"
+        self.inst_name = "sltiu"
+        self.inst_args = ["rd", "rs1", "imm"]
+
+        self.opcode = 0b0010011
+        self.func3 = 0b011
+        self.func7 = 0b0000000
+
+        self._build()
+
+    def description(self):
+        return f"R{self.rd} = (R{self.rs1} < {self.imm}) ? 1 : 0"
