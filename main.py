b1c729c26ec0a129f34fbc13010ecf9b8b948dcf
W-Mai <1341398182@qq.com>
2022-05-05 23:52:30 +0800

    测试：增加对新指令的测试代码

--- main.py
+++ main.py
@@ -5,7 +5,10 @@
 # Created Time:  2022-05-05
 #############################################
 
-from RiscVAssembler.ASMInst import ASMInstBase, ADD, SUB, XOR, OR, AND, SLL, SRL, SRA, SLT, SLTU
+from RiscVAssembler.ASMInst import (
+    ADD, SUB, XOR, OR, AND, SLL, SRL, SRA, SLT, SLTU,
+    ADDI, XORI, ORI, ANDI, SLLI, SRLI, SRAI, SLTI, SLTIU,
+)
 from RiscVAssembler.ASMBlock import ASMBlock
 
 
@@ -21,6 +24,20 @@ class ASMFile(ASMBlock):
         SRA(rd=29, rs1=27, rs2=29),
         SLT(rd=29, rs1=27, rs2=29),
         SLTU(rd=29, rs1=27, rs2=29),
+
+        ADDI(rd=27, rs1=0, imm=0b11011),
+        XORI(rd=29, rs1=27, imm=0b00101),
+        ORI(rd=29, rs1=27, imm=0b00101),
+        ANDI(rd=29, rs1=27, imm=0b00101),
+        SLLI(rd=29, rs1=27, imm=2),
+        SRLI(rd=29, rs1=27, imm=2),
+
+        ADDI(rd=27, rs1=0, imm=0xfff),
+        SRLI(rd=29, rs1=27, imm=2),
+        SRAI(rd=29, rs1=27, imm=2),
+        SLTI(rd=29, rs1=27, imm=0b11010),
+        SLTIU(rd=29, rs1=27, imm=0b11010),
+
     ]
 
 
